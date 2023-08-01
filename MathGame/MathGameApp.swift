923ad223825e4fb3fc7cb4494de17a235ef1f556
arif047 <rarifkhan652@gmail.com>
2023-08-01 10:56:19 +0600

    MathGameSwift
    MathGameSwift

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,13 @@
+//  Created by Arifur Rahman
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
